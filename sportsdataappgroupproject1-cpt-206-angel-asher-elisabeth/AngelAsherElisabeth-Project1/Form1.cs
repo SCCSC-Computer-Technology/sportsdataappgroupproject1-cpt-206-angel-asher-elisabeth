1415c577d8b5381f3c2413c620c6c0902faa1600
AngelDaDev <appsbyangel@gmail.com>
2026-02-28 16:36:52 -0500

    Added Data file and queries
    Need to hook into forms

---
+++ sportsdataappgroupproject1-cpt-206-angel-asher-elisabeth/AngelAsherElisabeth-Project1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AngelAsherElisabeth_Project1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoginRegister_Click(object sender, EventArgs e)
+        {
+            //Opens the RegisterForm
+            RegisterForm registerForm = new RegisterForm();
+            this.Hide(); 
+            registerForm.Show();
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            //Opens Customization form
+            CustomizationForm customizationForm = new CustomizationForm();
+            this.Hide();
+            customizationForm.Show();
+        }
+    }
+}
