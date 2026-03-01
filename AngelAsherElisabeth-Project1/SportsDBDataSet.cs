0461e02d66036792d4604b3cda3bd668f3d1673e
AngelDaDev <appsbyangel@gmail.com>
2026-02-28 21:16:03 -0500

    updated data and queries

---
+++ AngelAsherElisabeth-Project1/SportsDBDataSet.cs
@@ -0,0 +1,15 @@
+﻿namespace AngelAsherElisabeth_Project1
+{
+
+
+    partial class SportsDBDataSet
+    {
+    }
+}
+
+namespace AngelAsherElisabeth_Project1.SportsDBDataSetTableAdapters {
+    
+    
+    public partial class sportsTableAdapter {
+    }
+}
