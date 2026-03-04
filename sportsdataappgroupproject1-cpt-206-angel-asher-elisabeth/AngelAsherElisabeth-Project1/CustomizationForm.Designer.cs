5c0b37d0aecb35b7be2db2e832f2bb751cf31db0
AngelDaDev <appsbyangel@gmail.com>
2026-03-03 20:54:26 -0500

    added user class and additional queries

---
+++ sportsdataappgroupproject1-cpt-206-angel-asher-elisabeth/AngelAsherElisabeth-Project1/CustomizationForm.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace AngelAsherElisabeth_Project1
+{
+    partial class CustomizationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(282, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Account Settings";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(25, 111);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(725, 105);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Favorite Teams: ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Controls.Add(this.checkBox3);
+            this.groupBox2.Controls.Add(this.checkBox4);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Location = new System.Drawing.Point(25, 222);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(725, 105);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Notifcations:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.checkBox5);
+            this.groupBox3.Controls.Add(this.checkBox6);
+            this.groupBox3.Location = new System.Drawing.Point(25, 333);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(725, 105);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Display:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(90, 17);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(605, 82);
+            this.listBox1.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(90, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(97, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Score Updates";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(90, 67);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(153, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Upcoming Game Reminder";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(209, 19);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(91, 17);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "Start of Game";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(264, 67);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(119, 17);
+            this.checkBox4.TabIndex = 5;
+            this.checkBox4.Text = "End of Game Score";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(90, 32);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(79, 17);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "Dark Mode";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(235, 32);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(77, 17);
+            this.checkBox6.TabIndex = 5;
+            this.checkBox6.Text = "Large Font";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // CustomizationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "CustomizationForm";
+            this.Text = "CustomizationForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+    }
+}
---
+++ sportsdataappgroupproject1-cpt-206-angel-asher-elisabeth/AngelAsherElisabeth-Project1/User.cs
@@ -0,0 +1,94 @@
+﻿using AngelAsherElisabeth_Project1.SportsDBDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AngelAsherElisabeth_Project1
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+
+        public int Login(string username, string password)
+        {
+            try
+            {
+
+
+                SportsDBDataSet.usersDataTable users = new SportsDBDataSet.usersDataTable();
+
+                SportsDBDataSetTableAdapters.usersTableAdapter adapter = new SportsDBDataSetTableAdapters.usersTableAdapter();
+                DataTable records = adapter.GetLogin(username, password);
+
+                if (records.Rows.Count > 0)
+                {
+                    Id = (int)records.Rows[0]["Id"];
+                    return Id;
+                }
+                return -1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while logging in: " + ex.Message);
+                return -1;
+            }
+
+        }
+
+        public DataRow GetUser(int id)
+        {
+            try
+            {
+                SportsDBDataSetTableAdapters.usersTableAdapter adapter = new SportsDBDataSetTableAdapters.usersTableAdapter();
+                DataTable records = adapter.GetUserDetails(id);
+
+                if (records.Rows.Count > 0)
+                {
+                    return records.Rows[0];
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while fetching user details: " + ex.Message);
+            }
+            return null;
+        }
+
+        public int RegisterUser(string username, string password, string email)
+        {
+            try
+            {
+                SportsDBDataSetTableAdapters.usersTableAdapter adapter = new SportsDBDataSetTableAdapters.usersTableAdapter();
+
+                // The method returns the count of rows inserted
+                int rowsAffected = adapter.InsertUser(username, email, password);
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Success! User added. " + rowsAffected.ToString());
+                }
+                return rowsAffected;
+            }
+            catch (System.Data.SqlClient.SqlException ex)
+            {
+                // This catches database-specific errors (Duplicate IDs, Constraint issues)
+                MessageBox.Show("Database Error: " + ex.Message);
+                return -1;
+            }
+            catch (Exception ex)
+            {
+                // This catches everything else
+                MessageBox.Show("An error occurred: " + ex.Message);
+                return -1;
+            }
+
+        }
+    }
+}
